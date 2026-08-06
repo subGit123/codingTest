13884dcfa4fc027f8743aab9d795f56b6c71b3c7
subGit123 <mkkim6701@naver.com>
2026-08-06 18:33:30 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.11 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,5 @@
+function solution(phone_number) {
+    return phone_number.split("")    
+  .map((v, i) => (i < phone_number.length - 4 ? "*" : v))
+  .join("");  + phone_number.slice(phone_number.length - 4);
+}
